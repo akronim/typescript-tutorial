45a282893be1190157e36e872359bf6f40339634
acronim <marinko.miskovic.hr@gmail.com>
2020-12-10 22:13:09 +0100

    decorators

--- app.js
+++ app.js
@@ -1,28 +1,305 @@
-var Bad1;
-(function (Bad1) {
-    class Foo {
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+define("decorators", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.addMetadata = exports.readMetadata = exports.logProperty = exports.logMethod = exports.logClass = void 0;
+    function logClass(target) {
+        const originalConstructor = target;
+        function logClassName(func) {
+            console.log("New: " + func.name);
+        }
+        function instanciate(constructor, ...args) {
+            return new constructor(...args);
+        }
+        const newConstructor = function (...args) {
+            logClassName(originalConstructor);
+            return instanciate(originalConstructor, ...args);
+        };
+        newConstructor.prototype = originalConstructor.prototype;
+        return newConstructor;
     }
-    class Bar {
-        constructor() {
-            this.foo = new Foo();
+    exports.logClass = logClass;
+    function logMethod(target, key, descriptor) {
+        const originalMethod = descriptor.value;
+        function logFunctionCall(method, args, result) {
+            console.log(`Call: ${method}(${args}) => ${result}`);
+        }
+        descriptor.value = function (...args) {
+            const argsStr = args.map((a) => {
+                return JSON.stringify(a);
+            }).join();
+            const result = originalMethod.apply(this, args);
+            const resultStr = JSON.stringify(result);
+            console.log();
+            console.log(`Call: ${key}(${argsStr}) => ${resultStr}`);
+            return result;
+        };
+        return descriptor;
+    }
+    exports.logMethod = logMethod;
+    function logProperty(target, key) {
+        let _val = target[key];
+        function logPropertyAccess(acces, k, v) {
+            console.log(`${acces}: ${k} => ${v}`);
+        }
+        const getter = function () {
+            logPropertyAccess("Get", key, _val);
+            return _val;
+        };
+        const setter = function (newVal) {
+            logPropertyAccess("Set", key, newVal);
+            _val = newVal;
+        };
+        if (delete target[key]) {
+            Object.defineProperty(target, key, {
+                get: getter,
+                set: setter,
+                enumerable: true,
+                configurable: true
+            });
         }
     }
-    class Foobar {
-        constructor() {
-            this.foo = new Foo();
-            this.bar = new Bar();
+    exports.logProperty = logProperty;
+    function readMetadata(target, key, descriptor) {
+        const originalMethod = descriptor.value;
+        descriptor.value = function (...args) {
+            const metadataKey = `_log_${key}_parameters`;
+            const indices = target[metadataKey];
+            if (Array.isArray(indices)) {
+                for (let i = 0; i < args.length; i++) {
+                    if (indices.indexOf(i) !== -1) {
+                        const arg = args[i];
+                        const argStr = JSON.stringify(arg);
+                        console.log(`${key} arg[${i}]: ${argStr}`);
+                    }
+                }
+                return originalMethod.apply(this, args);
+            }
+        };
+        return descriptor;
+    }
+    exports.readMetadata = readMetadata;
+    function addMetadata(target, key, index) {
+        const metadataKey = `_log_${key}_parameters`;
+        if (Array.isArray(target[metadataKey])) {
+            target[metadataKey].push(index);
+        }
+        else {
+            target[metadataKey] = [index];
         }
     }
-})(Bad1 || (Bad1 = {}));
-var Better;
-(function (Better) {
-    class Foo {
+    exports.addMetadata = addMetadata;
+});
+define("01_class_decorator", ["require", "exports", "decorators"], function (require, exports, decorators_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var class_decorator_demo;
+    (function (class_decorator_demo) {
+        let Person = class Person {
+            constructor(name, surname) {
+                this.name = name;
+                this.surname = surname;
+            }
+            saySomething(something) {
+                return `${this.name} ${this.surname} says: ${something}`;
+            }
+        };
+        Person = __decorate([
+            decorators_1.logClass
+        ], Person);
+        const person = new Person("Michael", "Jackson");
+    })(class_decorator_demo || (class_decorator_demo = {}));
+});
+define("02_method_decorator", ["require", "exports", "decorators"], function (require, exports, decorators_2) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var method_decorator_demo;
+    (function (method_decorator_demo) {
+        class Person {
+            constructor(name, surname) {
+                this.name = name;
+                this.surname = surname;
+            }
+            saySomething(something) {
+                return `${this.name} ${this.surname} says: ${something}`;
+            }
+        }
+        __decorate([
+            decorators_2.logMethod
+        ], Person.prototype, "saySomething", null);
+        const person = new Person("Michael", "Jackson");
+        person.saySomething("Annie, are you ok?");
+    })(method_decorator_demo || (method_decorator_demo = {}));
+});
+define("03_property_decorator", ["require", "exports", "decorators"], function (require, exports, decorators_3) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var method_decorator_demo;
+    (function (method_decorator_demo) {
+        class Person {
+            constructor(name, surname) {
+                this.name = name;
+                this.surname = surname;
+            }
+            saySomething(something) {
+                return `${this.name} ${this.surname} says: ${something}`;
+            }
+        }
+        __decorate([
+            decorators_3.logProperty
+        ], Person.prototype, "name", void 0);
+        __decorate([
+            decorators_3.logProperty
+        ], Person.prototype, "surname", void 0);
+        const person = new Person("Michael", "Jackson");
+        person.saySomething("Annie, are you ok?");
+    })(method_decorator_demo || (method_decorator_demo = {}));
+});
+define("04_parameter_decorator", ["require", "exports", "decorators"], function (require, exports, decorators_4) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var method_decorator_demo;
+    (function (method_decorator_demo) {
+        class Person {
+            constructor(name, surname) {
+                this.name = name;
+                this.surname = surname;
+            }
+            saySomething(something) {
+                return `${this.name} ${this.surname} says: ${something}`;
+            }
+        }
+        __decorate([
+            decorators_4.readMetadata,
+            __param(0, decorators_4.addMetadata)
+        ], Person.prototype, "saySomething", null);
+        const person = new Person("Michael", "Jackson");
+        person.saySomething("Annie, are you ok?");
+    })(method_decorator_demo || (method_decorator_demo = {}));
+});
+var decorator_option;
+(function (decorator_option) {
+    function logClass(option) {
+        return function (target) {
+            console.log(`Decorating ${target.name} with ${option}`);
+        };
     }
-    class Bar {
-        constructor(foo) { }
+    let Person = class Person {
+    };
+    Person = __decorate([
+        logClass("some option")
+    ], Person);
+})(decorator_option || (decorator_option = {}));
+define("06_reflect_metadata", ["require", "exports", "../../../node_modules/reflect-metadata/reflect-metadata"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var reflect_metadata_demo1;
+    (function (reflect_metadata_demo1) {
+        function logType(target, key) {
+            const type = Reflect.getMetadata("design:type", target, key);
+            console.log(`${key} type: ${type.name}`);
+        }
+        class Demo {
+            constructor(attr1) {
+                this.attr1 = attr1;
+            }
+        }
+        __decorate([
+            logType
+        ], Demo.prototype, "attr1", void 0);
+    })(reflect_metadata_demo1 || (reflect_metadata_demo1 = {}));
+    var reflect_metadata_demo2;
+    (function (reflect_metadata_demo2) {
+        function logParamTypes(target, key) {
+            const types = Reflect.getMetadata("design:paramtypes", target, key);
+            const s = types.map((a) => a.name).join();
+            console.log(`${key} param types: ${s}`);
+        }
+        class Foo {
+        }
+        class Demo {
+            doSomething(param1, param2, param3, param4, param5, param6, param7) {
+                return 1;
+            }
+        }
+        __decorate([
+            logParamTypes
+        ], Demo.prototype, "doSomething", null);
+    })(reflect_metadata_demo2 || (reflect_metadata_demo2 = {}));
+    var reflect_metadata_demo3;
+    (function (reflect_metadata_demo3) {
+        function logReturntype(target, key) {
+            const returnType = Reflect.getMetadata("design:returntype", target, key);
+            console.log(`${key} return type: ${returnType.name}`);
+        }
+        class Demo {
+            doSomething2() {
+                return "test";
+            }
+        }
+        __decorate([
+            logReturntype
+        ], Demo.prototype, "doSomething2", null);
+    })(reflect_metadata_demo3 || (reflect_metadata_demo3 = {}));
+});
+define("07_decorator_factory", ["require", "exports", "decorators"], function (require, exports, decorators_5) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function decoratorFactory(classDecorator, propertyDecorator, methodDecorator, parameterDecorator) {
+        return function (...args) {
+            const nonNullableArgs = args.filter(a => a !== undefined);
+            switch (nonNullableArgs.length) {
+                case 1:
+                    return classDecorator.apply(this, args);
+                case 2:
+                    propertyDecorator.apply(this, args);
+                    break;
+                case 3:
+                    if (typeof args[2] === "number") {
+                        parameterDecorator.apply(this, args);
+                    }
+                    else {
+                        return methodDecorator.apply(this, args);
+                    }
+                    break;
+                default:
+                    throw new Error("Decorators are not valid here!");
+            }
+        };
     }
-    class Foobar {
-        constructor(foo, bar) { }
-    }
-    const foobar = new Foobar(new Foo(), new Bar(new Foo()));
-})(Better || (Better = {}));
+    var decorator_factory_demo;
+    (function (decorator_factory_demo) {
+        const log = decoratorFactory(decorators_5.logClass, decorators_5.logProperty, decorators_5.readMetadata, decorators_5.addMetadata);
+        let Person = class Person {
+            constructor(name, surname) {
+                this.name = name;
+                this.surname = surname;
+            }
+            saySomething(something) {
+                return `${this.name} ${this.surname} says: ${something}`;
+            }
+        };
+        __decorate([
+            log
+        ], Person.prototype, "name", void 0);
+        __decorate([
+            log,
+            __param(0, log)
+        ], Person.prototype, "saySomething", null);
+        Person = __decorate([
+            log
+        ], Person);
+        const person = new Person("Michael", "Jackson");
+        person.saySomething("Annie, are you ok?");
+    })(decorator_factory_demo || (decorator_factory_demo = {}));
+});
+//# sourceMappingURL=app.js.map
--- src/Exercises/e1.ts
+++ src/Exercises/e1.ts
@@ -64,3 +64,235 @@ interface B extends Omit<A, "x"> {
 }
 
 const t: B = { x: 3, y: "Y" };
+
+// e6
+(function () {
+  type AllKeyOf<T> = T extends never ? never : keyof T;
+
+  type Omit<T, K> = { [P in Exclude<keyof T, K>]: T[P] };
+
+  type Optional<T, K> = { [P in Extract<keyof T, K>]?: T[P] };
+
+  type WithOptional<T, K extends AllKeyOf<T>> = T extends never
+    ? never
+    : Omit<T, K> & Optional<T, K>;
+
+  /**
+    {
+         mid?: number;
+         type: number;
+         text?: string; 
+    }
+  | {
+         mid?: number;
+         type: number;
+         url: string;
+    }    
+  */
+  interface Message {
+    mid: string;
+    type: string;
+    text: string;
+  }
+
+  type a = WithOptional<Message, "mid" | "text">;
+
+  const optionalFoo: a = { mid: "???", type: "+++", text: "!!!" };
+
+  let msg: AllKeyOf<Message>;
+
+  //let keys = Object.keys(msg);
+
+  type MsgKeys = keyof Message;
+
+  let optionalFoo2: a;
+  function prop<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+  }
+})();
+
+(function () {
+  interface Animal {
+    extinct: boolean;
+    race: string;
+  }
+
+  function setValue<T, K extends keyof T>(obj: T, key: K, value: T[K]) {
+    obj[key] = value;
+  }
+
+  const huntsman: Animal = {
+    extinct: false,
+    race: "spider",
+  };
+
+  // true is a boolean value and matches the type of the "extinct" property
+  // --> no error
+  setValue(huntsman, "extinct", true);
+  setValue(huntsman, "race", "spider");
+
+  // 100 is a number value and does not match the type of the "extinct" property
+  // --> error
+  //setValue(huntsman, "extinct", 100);
+})();
+
+(function () {
+  interface IUserProfile {
+    id: string;
+    name: string;
+  }
+  type KeysEnum<T> = { [P in keyof Required<T>]: true };
+  const IUserProfileKeys: KeysEnum<IUserProfile> = {
+    id: true,
+    name: true,
+  };
+  console.log(IUserProfileKeys);
+})();
+
+(function () {
+  class MyTableClass {
+    // list the propeties here, ONLY WRITTEN ONCE
+    constructor(
+      readonly id?: string,
+      readonly title?: string,
+      readonly isDeleted?: boolean
+    ) {}
+  }
+
+  console.log(Object.keys(new MyTableClass()));
+})();
+
+(function () {
+  function assoc<Type, Key extends keyof Type>(
+    obj: Type,
+    key: Key,
+    value: Type[Key]
+  ) {
+    return { ...obj, [key]: value };
+  }
+
+  type User = {
+    id?: number;
+    name?: string;
+    points?: number;
+  };
+
+  let user: User = {};
+
+  const updatedUserName = assoc(user, "name", "User Test A");
+  const updatedUserPoints = assoc(user, "points", 0);
+
+  console.log({ user });
+
+  // The following will examples result in TypeScript complaining:
+
+  //const updatedUserPoint = assoc(user, "point", 0);
+  // Argument of type '"point"' is not assignable to parameter of type '"id" | "name" | "points"'
+
+  //const updatedUserPointsAsString = assoc(user, "point", "0");
+  // Argument of type '"0"' is not assignable to parameter of type 'number'
+})();
+
+(function () {
+  interface Todo {
+    title: string;
+    description: string;
+    foo: string;
+    bar: number;
+    baz: boolean;
+  }
+
+  type PartialTodo = Partial<Omit<Todo, "foo" | "baz">>;
+
+  const todoReq = {
+    title: "organize desk",
+    description: "clear clutter",
+    foo: "2",
+    bar: 333,
+  };
+
+  const todoToUpdate = {
+    title: "aaa",
+    description: "bbb",
+  };
+
+  Object.keys(todoReq).map((key, index) => {
+    if (["foo", "bar"].indexOf(key) > -1) delete todoReq[key];
+  });
+
+  let newObj;
+
+  function updateTodo(todoToUpdate: PartialTodo, todoReq: PartialTodo) {
+    //return { ...(todoToUpdate as Partial<Todo>), ...(todoReq as Partial<Todo>)};
+    newObj = Object.assign(todoToUpdate, todoReq);
+  }
+
+  const todo2 = updateTodo(todoToUpdate, todoReq);
+
+  console.log({ newObj });
+
+  Object.keys(todoReq).map((k, i) => {});
+
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
+  console.log(getKeyByValue(todoReq, "organize desk"));
+})();
+
+(function () {
+  // const tuple = <T extends string[]>(...args: T) => args;
+  // const furniture = tuple("chair", "table", "lamp");
+  // type Furniture = typeof furniture[number];
+
+  // const arrr: string[] = Object.keys(furniture);
+  // console.log({arrr});
+
+  const furniture = ["chair", "table", "lamp"] as const;
+  type Furniture = typeof furniture[number];
+  const arrr: string[] = [...furniture] as string[];
+  console.log({ arrr });
+  console.log(arrr.indexOf("table"));
+
+  const doms = {
+    a: "",
+    div: "",
+    par: "",
+  };
+
+  type DOMType = keyof typeof doms;
+
+  const arr: string[] = Object.keys(doms);
+  console.log(arr.indexOf("div"));
+})();
+
+(function () {
+  interface Foo {
+    foo: string;
+    bar: number;
+    baz: boolean;
+  }
+
+  type PartialFoo = Partial<Pick<Foo, "foo" | "baz">>;
+
+  type FooKeys = keyof PartialFoo;
+
+  const req = { foo: "foo", bar: 1, baz: true };
+
+  let partialFoo: PartialFoo = {};
+
+  Object.keys(req).map((key, index) => {
+    let a: FooKeys = req[key];
+    console.log(a);
+  });
+
+  
+  console.log({ partialFoo });
+
+})();
+
+(function () {
+  let x = { a: 1, b: 2, c: 3, z: 26 };
+  let { b, ...y } = x;
+  console.log(y);
+})();
